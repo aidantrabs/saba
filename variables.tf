465b049a934ea7efc9df9cd65383cfaa11852a0d
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-01-15 00:45:06 -0400

    rename variables

--- variables.tf
+++ variables.tf
@@ -1,17 +1,29 @@
 variable "vpc_cidr" {
-    type = string
-    description = "VPC CIDR blcok"
-    default = "10.0.0.0/16"
+    type        = string
+    description = "VPC CIDR block"
+    default     = "10.0.0.0/16"
 }
 
 variable "environment" {
-    type = string
-    description = "Environment (Resource naming / tags)"
-    default = "dev"
+    type        = string
+    description = "Environment name for resource tagging"
+    default     = "dev"
 }
 
 variable "aws_region" {
-    type = string
-    description = "AWS provider region"
-    default = "us-east-1"
+    type        = string
+    description = "AWS region"
+    default     = "us-east-1"
+}
+
+variable "az_a" {
+    type        = string
+    description = "Availability Zone A"
+    default     = "us-east-1a"
+}
+
+variable "az_b" {
+    type        = string
+    description = "Availability Zone B"
+    default     = "us-east-1b"
 }
