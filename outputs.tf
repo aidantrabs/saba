1c1dc552af4a9fb22e8d1df7fb3aea87afa6dd86
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-01-15 00:44:51 -0400

    update ouputs + naming

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,34 @@
 output "vpc_id" {
     description = "ID of the VPC"
-    value = aws_vpc.main.id
+    value       = aws_vpc.main.id
 }
 
-output "aws_subnet_id" {
-    description = "ID of the subnet"
-    value = aws_subnet.main.id
+output "public_subnet_a_id" {
+    description = "ID of public subnet A"
+    value       = aws_subnet.public_a.id
+}
+
+output "public_subnet_b_id" {
+    description = "ID of public subnet B"
+    value       = aws_subnet.public_b.id
+}
+
+output "private_subnet_a_id" {
+    description = "ID of private subnet A"
+    value       = aws_subnet.private_a.id
+}
+
+output "private_subnet_b_id" {
+    description = "ID of private subnet B"
+    value       = aws_subnet.private_b.id
+}
+
+output "nat_gateway_id" {
+    description = "ID of the NAT Gateway"
+    value       = aws_nat_gateway.main.id
+}
+
+output "internet_gateway_id" {
+    description = "ID of the Internet Gateway"
+    value       = aws_internet_gateway.main.id
 }
